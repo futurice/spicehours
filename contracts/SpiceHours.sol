636f27b5ede3631bed9dae79d16fb191cac7b050
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2016-11-06 16:04:21 +0200

    Code style fixes to SpiceHours.

--- contracts/SpiceHours.sol
+++ contracts/SpiceHours.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.2;
 
 import "SpiceControlled.sol";
+import "IBalanceConverter.sol";
 import "SpicePayroll.sol";
 
 contract SpiceHours is SpiceControlled {
@@ -10,35 +11,36 @@ contract SpiceHours is SpiceControlled {
     event MarkHours(address indexed _sender, bytes32 indexed _info, bytes32 indexed _description, int _secs);
     event FixHours(address indexed _sender, bytes32 indexed _info, bytes32 indexed _description, int _secs);
 
-    function SpiceHours(address membersAddress) SpiceControlled(membersAddress) {
+    function SpiceHours(address _members) SpiceControlled(_members) {
         fromTimestamp = now;
     }
 
-    function adjustHours(bytes32 info, int secs) private {
-        if (info == 0) throw;
-        if (secs == 0) throw;
-        if (secs < 0 && balance[info] < uint(-secs)) throw;
+    function adjustHours(bytes32 _info, int _secs) private {
+        if (_info == 0) throw;
+        if (_secs == 0) throw;
+        if (_secs < 0 && balance[_info] < uint(-_secs)) throw;
 
-        if (secs < 0) {
-            balance[info] -= uint(-secs);
+        if (_secs < 0) {
+            balance[_info] -= uint(-_secs);
         } else {
-            balance[info] += uint(secs);
+            balance[_info] += uint(_secs);
         }
     }
 
-    function markHours(bytes32 info, bytes32 description, int secs) onlyMember {
-        if (!hasManagerAccess(msg.sender) && members.memberInfo(msg.sender) != info) throw;
+    function markHours(bytes32 _info, bytes32 _description, int _secs) onlyMember {
+        if (!hasManagerAccess(msg.sender) && members.memberInfo(msg.sender) != _info) throw;
 
-        adjustHours(info, secs);
-        MarkHours(msg.sender, info, description, secs);
+        adjustHours(_info, _secs);
+        MarkHours(msg.sender, _info, _description, _secs);
     }
 
-    function fixHours(bytes32 info, bytes32 description, int secs) onlyDirector {
-        adjustHours(info, secs);
-        FixHours(msg.sender, info, description, secs);
+    function fixHours(bytes32 _info, bytes32 _description, int _secs) onlyDirector {
+        adjustHours(_info, _secs);
+        FixHours(msg.sender, _info, _description, _secs);
     }
 
-    function processPayroll(address balanceConverter) onlyDirector {
+    function processPayroll(address _balanceConverter) onlyDirector {
+        IBalanceConverter converter = IBalanceConverter(_balanceConverter);
         SpicePayroll payroll = new SpicePayroll(msg.sender, fromTimestamp);
     }
 }
