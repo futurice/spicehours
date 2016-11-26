18db1a83f59d68da41a7f7a51600a5010768421f
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2016-11-26 17:44:14 +0200

    Add date to the beginning of events.

--- server/public/main.js
+++ server/public/main.js
@@ -6,25 +6,28 @@
   // See https://gist.github.com/dperini/729294
   var urlRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
 
+  var dateFormatter = new Intl.DateTimeFormat({ year: 'numeric', month: '2-digit', day: '2-digit' });
+
   var EventItem = React.createClass({
     propTypes: {
       event: React.PropTypes.object.isRequired
     },
     render: function() {
       var event = this.props.event;
+      var date = dateFormatter.format(event.block ? new Date(event.block.timestamp * 1000) : new Date());
       var name = (event.user ? event.user.first_name + ' ' + event.user.last_name : event.args.info);
       if (event.event === 'MarkHours') {
         return React.createElement('li', { key: eventId(event) },
-          name + ' marked ' + moment.duration(event.args.duration*1000).humanize() + ' to project ' + event.args.description +
+          date + ' ' + name + ' marked ' + moment.duration(event.args.duration*1000).humanize() + ' to project ' + event.args.description +
           (event.args.success ? '' : ' FAILED')
         );
       } else if (event.event === 'ProcessPayroll') {
         return React.createElement('li', { key: eventId(event) },
-          'Payroll was processed, started new payroll'
+          date + ' Payroll was processed, started new payroll'
         );
       } else {
         return React.createElement('li', { key: eventId(event) },
-          'Unknown event ' + event.event
+          date + ' Unknown event ' + event.event
         );
       }
     }
