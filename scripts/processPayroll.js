119842ae6f866fa43436dc17018afc41ed570eda
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2016-12-29 12:16:23 +0200

    Add much higher gas limit to processPayroll script.

--- scripts/processPayroll.js
+++ scripts/processPayroll.js
@@ -8,7 +8,7 @@ eth.prepare().then(() => {
   const rates = SpiceRates.deployed();
 
   console.log('Processing payroll');
-  return hours.processPayroll(rates.address, 30*60*60, {from: '0x6799A1D5F574eF1C376F5515eE7e2B8b06B30754', gas: 1000000 });
+  return hours.processPayroll(rates.address, 30*60*60, {from: '0x6799A1D5F574eF1C376F5515eE7e2B8b06B30754', gas: 3141592 });
 }).then(() => {
   console.log('Processed payroll successfully');
 }).catch(err => console.log(err));
